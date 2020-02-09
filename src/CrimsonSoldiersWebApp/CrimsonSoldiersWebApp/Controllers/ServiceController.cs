d56d8675384db600aa8fff3797b8ba2c527641dc
Junet Bello <junete985@gmail.com>
2020-02-09 01:11:15 -0500

    Initial Prototype
    Includes all the necessary components of the app.

---
+++ src/CrimsonSoldiersWebApp/CrimsonSoldiersWebApp/Controllers/ServiceController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrimsonSoldiersWebApp.Controllers
+{
+     [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+    {
+    }
+}
---
+++ src/CrimsonSoldiersWebApp/CrimsonSoldiersWebApp/Infrastructure/Data/CrimsonContext.cs
@@ -0,0 +1,15 @@
+﻿using CrimsonSoldiersWebApp.ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrimsonSoldiersWebApp.Infrastructure.Data
+{
+     public class CrimsonContext : DbContext
+     {
+          public CrimsonContext(DbContextOptions<CrimsonContext> options)
+            : base(options)
+          {
+          }
+
+          public DbSet<Service> Services { get; set; }
+     }
+}
